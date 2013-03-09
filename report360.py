866ece19ce4969fa2b752e7c026e2ac56c3a98ff
Alex Nelson <ajnelson@cs.ucsc.edu>
2013-03-08 23:38:59 -0800

    Expose each file's flags in DFXML to compare parsings

--- report360.py
+++ report360.py
@@ -99,6 +99,7 @@ class Report360:
                 dfxml_fn = "/".join(dfxml_fn_parts)
                 self.xprint("      <filename>%s</filename>" % dfxml_fn) #DFXML filenames omit the leading "/"
             self.xprint("      <py360:filename>%s</py360:filename>" % filename[1:]) #DFXML filenames omit the leading "/"
+            self.xprint("      <xtaf:flags>%r</xtaf:flags>" % fr.attribute)
 #      <partition>1</partition>
             self.xprint("      <id>%d</id>" % FILE_ID_COUNTER)
             FILE_ID_COUNTER += 1
